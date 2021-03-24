612853dd39fadc6431d1b38f5079bb8a4d713301
ohaiklochan <caforonda103@yahoo.com>
2021-03-24 18:16:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+
 // Code your solution here
+
+function findMatching(drivers, string) {
+    return drivers.filter(find_string => {
+        return(find_string.toLowerCase() === string.toLowerCase())
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(find_string => {
+        if (string.charAt(0) === find_string.charAt(0)) {
+            return find_string
+        }
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function(find_name) {
+        if (find_name.name == name) {
+            return find_name
+        }
+    })
+}
